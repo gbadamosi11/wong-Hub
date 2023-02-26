fa12448c0142e3dab7519c06649e28982bcfb671
gbadamosi11 <gbadamosimubarak60@gmail.com>
2023-02-26 11:47:12 +0100

    Shop and single product page

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ if(close){
         nav.classList.remove('active');
     });
 }
+
+
+var MainImg = document.getElementById("MainImg")
+var smalling = document.getElementsByClassName("small-img");
+
+smalling[0].onclick = function(){
+    MainImg.src = smalling[0].src;
+}
+smalling[1].onclick = function(){
+    MainImg.src = smalling[1].src;
+}
+smalling[2].onclick = function(){
+    MainImg.src = smalling[2].src;
+}
+smalling[3].onclick = function(){
+    MainImg.src = smalling[3].src;
+}
